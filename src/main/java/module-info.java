a9e29ced4ace0099b5d100848f51b4c1f773c847
calinathalya <calina.thalya@icloud.com>
2023-10-03 22:43:55 -0300

    atualizando

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,10 @@ module com.example {
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
 
+    requires transitive javafx.graphics;
+
     opens com.example to javafx.fxml;
     exports com.example;
 }
+
+
